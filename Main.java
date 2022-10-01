3ab3f617327ba7dabe992944ecde1c9fce61aa5e
Yuika S <>
2022-10-01 21:11:42 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+class Main {
+  public static void main(String[] args) {
+    
+    
+    printToNum();
+    
+  }
+  
+public static void printToNum(){
+System.out.println ((int) (Math.random()*(8-1+1)+1));
+  }
+}
+  
+  
